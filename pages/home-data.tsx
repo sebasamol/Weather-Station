b77d062d7b43f1f3136ae7e078530231362df2ea
sebasamol <sebasamol@gmail.com>
2025-07-20 21:42:34 +0200

    Update HomeData component to include logo and improve layout
    - Added a clickable logo image to the HomeData component for better navigation.
    - Enhanced layout with flexbox for improved alignment and responsiveness of HomeCharts.

--- pages/home-data.tsx
+++ pages/home-data.tsx
@@ -1,11 +1,25 @@
 'use client';
 import React from 'react';
 import HomeCharts from '../app/components/HomeCharts/HomeCharts';
+import Image from 'next/image';
 import '../app/globals.css';
 export default function HomeData() {
     return (
-        <div>
-            <HomeCharts />
+        <div className="flex flex-col items-center justify-center w-screen px-2 sm:px-4 md:px-6 py-2 sm:py-4 overflow-x-hidden">
+            <div className="w-full max-w-1xl ">
+                <a href="/">
+                    <Image
+                        src="/home.png"
+                        alt="Home"
+                        width={36}
+                        height={36}
+                        className="cursor-pointer"
+                    />
+                </a>
+            </div>
+            <div className="w-full max-w-6xl">
+                <HomeCharts />
+            </div>
         </div>
     );
 }
